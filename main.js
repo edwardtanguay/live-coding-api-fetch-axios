51b628e8f8f8ad4273d9fea0e915baf022781629
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-04 10:31:19 +0200

    data is fetched

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 import './style.scss'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const url = 'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json';
+
+(async () => {
+	const response = await fetch(url);
+	const employees = await response.json();
+	document.querySelector('#app').innerHTML = `
+  <h1>Employees</h1>
+  <ul>
+      ${employees.map(employee => {
+		  return `<li>${employee.firstName} ${employee.lastName}</li>`;
+	  }).join('')} 
+  </ul>
+`;
+})();
+
