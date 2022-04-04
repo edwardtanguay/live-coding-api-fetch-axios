50bf76396b25c7f00289e0797f1f4c75490717e6
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-04 10:54:03 +0200

    axios implemented

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 import './style.scss'
+import axios from 'axios';
 
 const url = 'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json';
 
 (async () => {
-	const response = await fetch(url);
-	const employees = await response.json();
+	
+	// fetch
+	// const response = await fetch(url);
+	// const employees = await response.json();
+
+	// axios
+	const response = await axios.get(url);
+	const employees = response.data;
+
 	document.querySelector('#app').innerHTML = `
   <h1>Employees</h1>
   <ul>
